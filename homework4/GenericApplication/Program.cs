bfc44a248d4126b50b7f4208caa6d4fba41eeba4
weihaohuang-1998 <1328371390@qq.com>
2020-03-18 23:41:04 +0800

    第四次课后作业

---
+++ homework4/GenericApplication/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> temp = head;
+            while (temp.Next != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+            action(temp.Data);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+            Console.WriteLine();
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+            Console.WriteLine();
+            intlist.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+            int sum = 0, min = 0, max = 0;
+            intlist.ForEach(x => sum += x);
+            intlist.ForEach(x => { if (max < x) max = x; });
+            intlist.ForEach(x => { if (min > x) min = x; });
+            Console.WriteLine(sum);
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+
+        }
+
+    }
+}
---
+++ homework4/Timer/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace test2
+{
+    public delegate void TickHandler();
+    public delegate void AlarmHandler();
+
+    public class Clock
+    {
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+        private List<Alarm> AlarmlList = new List<Alarm>();
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Clock()
+        {
+            this.hour = 0;
+            this.minute = 0;
+            this.second = 0;
+            OnTick += new TickHandler(timeReporter);
+            OnAlarm += Ring;
+        }
+
+        public Clock(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+            OnTick += new TickHandler(timeReporter);
+            OnAlarm += Ring;
+        }
+
+        public void start()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                this.second++;
+                OnTick();
+                for (int i = 0; i < AlarmlList.Count; i++)
+                {
+                    if (AlarmlList[i].isTimeToAlarm(this.hour, this.minute, this.second))
+                        OnAlarm();
+                }
+            }
+        }
+
+        public void setAlarm(int h, int m, int s)
+        {
+            Alarm a = new Alarm(h, m, s);
+            AlarmlList.Add(a);
+        }
+
+        public void Ring()
+        {
+            Console.WriteLine("This is a alarm!");
+        }
+
+        public void timeReporter()
+        {
+            Console.WriteLine(this.hour + ":" + this.minute + ":" + this.second);
+            if (this.second == 60)
+            {
+                this.second = 0;
+                this.minute++;
+            }
+            if (this.minute == 60)
+            {
+                this.minute = 0;
+                this.hour++;
+            }
+            if (this.hour == 24)
+                this.hour = 0;
+        }
+
+    }
+
+
+    class Alarm
+    {
+        public int hour;
+        public int minute;
+        public int second;
+        public Alarm(int h, int m, int s)
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+        public bool isTimeToAlarm(int h, int m, int s)
+        {
+            if (hour == h && minute == m && second == s)
+                return true;
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock(23, 25, 20);
+            myClock.setAlarm(23, 25, 30);
+            myClock.setAlarm(23, 25, 40);
+            myClock.setAlarm(23, 25, 50);
+            myClock.start();
+        }
+    }
+}
