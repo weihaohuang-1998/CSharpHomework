ad251cf67b17c3dddef15a002d5532a39757f3bb
weihaohuang-1998 <1328371390@qq.com>
2020-04-23 19:46:06 +0800

    第九次课后作业

---
+++ homework9/SimpleCrawler/SimpleCrawler/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ConsoleApp3
+{
+    class SimpleCrawler
+    {
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        static private string startUrl = "https://www.cnblogs.com/dstang2000/";
+        static void Main(string[] args)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            if (args.Length >= 1) startUrl = args[0];
+            myCrawler.urls.Add(startUrl, false);//加入初始页面
+            new Thread(myCrawler.Crawl).Start();
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                Parse(html, current);//解析,并加入新的链接
+                Console.WriteLine("爬行结束");
+            }
+            Console.ReadLine();
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html, string Url)
+        {
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'](http|https)[^""'#>]+..html.*?[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0)
+                    continue;
+                if (url.Contains("https://www.cnblogs.com"))
+                {
+                    if (urls[url] == null)
+                        urls[url] = false;
+                }
+            }
+
+            strRef = @"(href|HREF)[ ]*=[ ]*[""'][^(http|https)][^""'#>]+..html.*?[""']";
+            matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0) continue;
+                Uri baseUri = new Uri(Url);
+                Uri absoluteUri = new Uri(baseUri, url);
+                Console.WriteLine("相对:" + url);
+                Console.WriteLine("绝对:" + absoluteUri.ToString());
+                if (urls[absoluteUri.ToString()] == null)
+                    urls[absoluteUri.ToString()] = false;
+            }
+        }
+    }
+}
+
+
